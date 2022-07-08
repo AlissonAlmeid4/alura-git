916304c256c47e13028a1fedcdad14a9062fa7c6
Alisson <alisson.a-work@outlook.com>
2022-07-08 15:31:12 -0300

    json no html

---
+++ Proc_Seletivo/Projetos/Backend/caduser.js
@@ -0,0 +1,18 @@
+const cadastro = require('./caduser.json')
+
+function Listar (cadastro){
+    return;
+}
+
+//     return "Alisson"   
+// }
+// function deleteUser(CPF){
+//     return "Usuario deletado"
+// }
+// var list = function getAllUser(){
+//     return "Lista"
+// }
+
+
+module.exports = cadastro
+// module.exports = list
