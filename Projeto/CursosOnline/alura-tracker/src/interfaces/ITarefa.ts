d5a96aef88acf0b66a59ba4f4d7b4e199f4b1a53
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2022-07-16 13:26:10 -0300

    subindo nuvem

--- Projeto/CursosOnline/alura-tracker/src/interfaces/ITarefa.ts
+++
@@ -1,7 +0,0 @@
-import IProjeto from "./IProjeto";
-
-export default interface Itarefa {
-    duracaoEmSegundos: number,
-    descricao: string
-    projeto: IProjeto
-}
--- Projeto/Praticando/UsandoJson/UsandoJson.js
+++
@@ -1,9 +0,0 @@
-const dados = {	"nome": "Pedro", "altura": 1.90, "idade": 15 }
-
-//console.log(typeof dados);
-let texto = JSON.stringify(dados)
-//console.log(typeof texto);
-
- //console.log(dados.nome)
-// console.log( dados.altura)
-export default dados;
