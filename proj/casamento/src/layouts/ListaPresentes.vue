6dc5ab5facf6f41a5d09df4bb5fe670ca162d53c
Alisson <alisson@rosset.com.br>
2022-08-17 12:13:37 -0300

    lista de presentes

--- proj/casamento/src/layouts/ListaPresentes.vue
+++ proj/casamento/src/layouts/ListaPresentes.vue
@@ -1,15 +1,62 @@
 <template>
-<div class="q-pa-md">
+<div class="q-pa-md ">
     <div class="mensagem ">
         <div class="text-h2">PRESENTES</div>
     </div>
-        <div class="mensagem">
-        <div class="text-subtitle1 " >
+<div class="lista">
+  <div class="q-pa-md ">
+    <div class="column items-center" style="height: 300px; max-height: 100%">
+      <div class="col-8">
 <pre>
-
+•	Batedeira
+•	Cafeteira
+•	Ferro de passar
+•	Fogão
+•	Microondas
+•	Geladeira
+•	Liquidificador
+•	Máquina de Lavar roupa
+•	Multiprocessador
+•	Sanduicheira elétrica
+•	Ventilador
+•	Faqueiro
+•	Garrafa térmica
+•	Jogos de facas para cozinha (manteiga e patê)
+•	Bule açucareiro e Leiteira
+•	Jogo de sobremesa
+•	Tábua para passar roupa
+•	Tábuas carne
+•	Conjunto de copos ou taças
+•	Bandejas diversas
+•	Fruteiras
 </pre>
-          </div>
+      </div>
+      <div class="col-5">
+<pre>
+•	Porta-condimentos
+•	Jarra
+•	Conjunto de assadeiras
+•	Chaleira
+•	Panelas antiaderentes
+•	Formas de bolo, pudim, suflê e torta
+•	Jogo de panelas
+•	Leiteira
+•	Panela caçarola
+•	Panela de pressão
+•	Cama
+•	Guarda-roupa
+•	Mesa de Jantar
+•	Sofá
+•	Sapateira
+•	Criado mudo
+•	Mesa de centro
+•	Prateleiras
+</pre>
+      </div>
     </div>
+
+  </div>
+  </div>
 </div>
 </template>
 
@@ -17,6 +64,7 @@
 export default {
     name: 'ListaPresentes',
 }
+
 </script>
 
 <style>
@@ -24,7 +72,5 @@ export default {
   text-align: center;
   padding: 12px;
   color: rgba(0, 0, 0, .7);
-
   }
-
 </style>
