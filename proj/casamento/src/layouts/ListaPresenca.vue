187db315ebfef366ee3a1e33add86f2cb7bd175b
Alisson <alisson@rosset.com.br>
2022-08-19 09:31:47 -0300

    asa

---
+++ proj/casamento/src/layouts/ListaPresenca.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="q-pa-md column items-center">
+
+      <div class="mensagem ">
+          <div class="text-h4">CONFIRMAÇÃO DE PRESENÇA</div>
+      </div>
+      <div class="mensagem">
+          <div class="text-h6">FAÇA PARTE DA NOSSA HISTÓRIA, CONFIRME SUA PRESENÇA.</div>
+      </div>
+  <div class="q-pa-md row justify-center">
+          <img src="../photos/casal.png"  width="200px" height="200px" alt="" srcset="">
+      </div>
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScln3q-4S1y07I9QAHZx6pBeBkJnJGV5Z83cwjkqpmMIzgcIQ/viewform?embedded=true"
+  width="840" height="850" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ListaPresenca',
+
+}
+</script>
+
+<style>
+
+</style>
