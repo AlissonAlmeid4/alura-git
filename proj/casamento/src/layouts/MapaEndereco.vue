145943ef1b89d2088bd0dd19ca15957f26b5c15c
Alisson <alisson@rosset.com.br>
2022-12-21 15:28:19 -0300

    atualizado

--- proj/casamento/src/layouts/MapaEndereco.vue
+++ proj/casamento/src/layouts/MapaEndereco.vue
@@ -30,7 +30,7 @@
             </div>
           </div>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d7778380.094000022!2d-47.850987212669935!3d-17.836893084205936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d-23.462016199999997!2d-46.4681595!4m5!1s0x715ca52aadb5131%3A0xe43ba42246956382!2sEstr.%20p%2F%20Fl%C3%B4res%2C%20107%20-%20S%C3%A3o%20Gon%C3%A7alo%20dos%20Campos%2C%20BA%2C%2044330-000!3m2!1d-12.445787699999999!2d-38.947165!5e0!3m2!1spt-BR!2sbr!4v1660820791736!5m2!1spt-BR!2sbr"
+            src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d124719.95493251801!2d-38.98227929599896!3d-12.349498661437257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x714378df10f9afb%3A0xb5f00f82796f4aa6!2sFeira%20de%20Santana%20-%20BA!3m2!1d-12.2535651!2d-38.9600273!4m5!1s0x715ca52aadb5131%3A0xe43ba42246956382!2sEstr.%20p%2F%20Fl%C3%B4res%2C%20107%20-%20S%C3%A3o%20Gon%C3%A7alo%20dos%20Campos%2C%20BA%2C%2044330-000!3m2!1d-12.445787699999999!2d-38.947165!5e0!3m2!1spt-BR!2sbr!4v1671647076898!5m2!1spt-BR!2sbr"
             width="100%"
             height="650"
             style="border: 0"
@@ -59,7 +59,7 @@
             </div>
           </div>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d117175.94636338859!2d-46.405002019573644!3d-23.397320297350294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d-23.462016199999997!2d-46.4681595!4m5!1s0x94ce81efe01073b5%3A0x5403da6106fb371b!2sSanta%20Isabel%2C%20SP!3m2!1d-23.316603399999998!2d-46.2261354!5e0!3m2!1spt-BR!2sbr!4v1660820417171!5m2!1spt-BR!2sbr"
+            src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d117182.01241821653!2d-46.38984531970749!3d-23.390464142701205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x94ce64636302db79%3A0xef5ae20eb54ed329!2sJardim%20Nazareth%2C%20S%C3%A3o%20Paulo%20-%20SP!3m2!1d-23.5179677!2d-46.406875199999995!4m5!1s0x94ce855feb8157c5%3A0xf07d64f0be23d65e!2s07500-000%2C%20Santa%20Isabel%20-%20SP!3m2!1d-23.2618673!2d-46.2474381!5e0!3m2!1spt-BR!2sbr!4v1671646862712!5m2!1spt-BR!2sbr"
             width="100%"
             height="650"
             style="border: 0"
