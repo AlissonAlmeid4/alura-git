b522131d7cf582b757d3660aa97d57cb82b7138b
Alisson <alisson@rosset.com.br>
2022-12-21 07:14:11 -0300

    atualizando menu

--- proj/casamento/src/layouts/MainLayout.vue
+++ proj/casamento/src/layouts/MainLayout.vue
@@ -32,90 +32,85 @@
     </q-drawer>
 
     <q-page-container>
-	 <router-view />
-   <FooTer/>
+      <router-view />
     </q-page-container>
 
     <q-page-scroller
       position="bottom-right"
       :scroll-offset="150"
       :offset="[18, 18]"
-
     >
       <q-btn fab icon="keyboard_arrow_up " color="green" />
     </q-page-scroller>
   </q-layout>
-
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-import FooTer from './FooTer.vue'
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
 
 const linksList = [
   {
-    title: 'Inicio',
-    caption: 'Bem-Vindo',
-    icon: 'home',
-    link: '/'
+    title: "Inicio",
+    caption: "Bem-Vindo",
+    icon: "home",
+    link: "/AlissonAlmeid4/#",
   },
   {
-    title: 'Sobre',
-    caption: 'Nossa História',
-    icon: 'favorite',
-    link: '/#/about'
+    title: "Sobre",
+    caption: "Nossa História",
+    icon: "favorite",
+    link: "/AlissonAlmeid4/#/about",
   },
   {
-    title: 'Presentes',
-    caption: 'Sujestão de Presente',
-    icon: 'link',
-    link: '/#/present'
+    title: "Presentes",
+    caption: "Sujestão de Presente",
+    icon: "link",
+    link: "/AlissonAlmeid4/#/present",
   },
   {
-    title: 'Lista de presença',
-    caption: 'Confirme sua presença',
-    icon: 'list',
-    link: '/#/gift'
+    title: "Lista de presença",
+    caption: "Confirme sua presença",
+    icon: "record_voice_over",
+    link: "/AlissonAlmeid4/#/gift",
   },
   {
-    title: 'O Casamento',
-    caption: 'Como chegar',
-    icon: 'map',
-    link: '/#/maps'
+    title: "O Casamento",
+    caption: "Como chegar",
+    icon: "map",
+    link: "/AlissonAlmeid4/#/maps",
   },
   {
-    title: 'Contato',
-    caption: 'Fale com a Gente',
-    icon: 'public',
-    link: '/#/contact'
+    title: "Contato",
+    caption: "Fale com a Gente",
+    icon: "public",
+    link: "/AlissonAlmeid4/#/contact",
   },
   {
-    title: 'Apoie-nos',
-    caption: 'Transferencia',
-    icon: '$',
-    link: '/#/transfer'
-  }
-]
+    title: "Apoie-nos",
+    caption: "Transferencia",
+    icon: "$",
+    link: "/AlissonAlmeid4/#/transfer",
+  },
+];
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
     EssentialLink,
-    FooTer
-},
+  },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
       essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
 </script>
