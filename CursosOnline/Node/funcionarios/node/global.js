3221dcd6fcc6c3bcb85cccee77b2049d933b2001
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2023-08-27 15:10:44 -0300

    curso

---
+++ CursosOnline/Node/funcionarios/node/global.js
@@ -0,0 +1,7 @@
+//console.log(global);
+globalThis.MinhaAPP = Object.freeze({ //sem o Object.freeze é possivel alterar essa função em quanquer arquivo
+    saudacao(){
+    return 'Estou em todos os lugares!'
+    }, 
+    nome: 'Sistema legal'
+ })
