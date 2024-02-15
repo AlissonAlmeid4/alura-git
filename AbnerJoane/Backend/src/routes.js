d0ec6f08c6edba843aecd5574abfd881dc8508f2
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2024-02-14 21:43:48 -0300

    BackEnd OK

--- AbnerJoane/Backend/src/routes.js
+++ AbnerJoane/Backend/src/routes.js
@@ -5,6 +5,20 @@ const casamentoController = require('./controllers/casamentoController')
 
 router.get('/pessoa', casamentoController.buscarTodos);
 router.get('/pessoa/:codigo', casamentoController.buscarUm);
+router.post('/pessoa', casamentoController.inserir);
+router.put('/pessoa/:codigo', casamentoController.alterar);
+router.delete('/pessoa/:codigo', casamentoController.excluir);
+
+router.get('/presente', casamentoController.buscarTodosPresente);
+router.get('/presente/:nome', casamentoController.buscarUmPresente);
+router.post('/presente', casamentoController.inserirPresente);
+router.delete('/presente/:codigo', casamentoController.excluirPresente);
+
+router.get('/mural', casamentoController.buscarTodosMural);
+router.get('/mural/:codigo', casamentoController.buscarUmMural);
+router.post('/mural', casamentoController.inserirMural);
+router.put('/mural/:codigo', casamentoController.alterarMural);
+router.delete('/mural/:codigo', casamentoController.excluirMural);
 
 module.exports = router;
 
--- AbnerJoane/Backend/src/services/casamentoService.js
+++ AbnerJoane/Backend/src/services/casamentoService.js
@@ -20,5 +20,134 @@ module.exports = {
                 }
             })
         })
+    },
+    inserir: (nome, celular, tipo) => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('INSERT INTO pessoa (nome, celular, tipo)  VALUES (?,?,?)',
+                [nome, celular, tipo], 
+                (error, results)=>{
+                    if(error){ rejeitado(error); return; }
+                        aceito(results.insertCodigo);
+            })
+        })
+    },
+    alterar: (codigo, nome, celular, tipo) => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('UPDATE pessoa SET nome = ?, celular = ?, tipo = ? WHERE codigo = ?',
+                [nome, celular, tipo, codigo], 
+                (error, results)=>{
+                    if(error){ rejeitado(error); return; }
+                        aceito(results);
+            })
+        })
+    },
+    excluir: (codigo) => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('DELETE FROM pessoa WHERE codigo = ?',
+                [codigo], 
+                (error, results)=>{
+                    if(error){ rejeitado(error); return; }
+                        aceito(results);
+            })
+        })
+    },
+
+
+    //presentes
+
+    buscarTodosPresente: ()=>{
+        return new Promise((aceito, rejeitado)=>{
+
+            db.query('SELECT * FROM presente', (error, results)=>{
+                if(error){ rejeitado(error); return; }
+                aceito(results)
+            });
+        });
+    },
+    buscarUmPresente: (nome) => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('SELECT * FROM presente WHERE nome = ?', [nome], (error, results)=>{
+                if(error){ rejeitado(error); return; }
+                if(results.length > 0){
+                    aceito(results[0]);
+                } else{
+                    aceito(false);
+                }
+            })
+        })
+    },
+    inserirPresente: (nome, celular, presente) => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('INSERT INTO presente (nome, celular, presente)  VALUES (?,?,?)',
+                [nome, celular, presente], 
+                (error, results)=>{
+                    if(error){ rejeitado(error); return; }
+                        aceito(results.insertCodigo);
+            })
+        })
+    },
+    excluirPresente: (codigo) => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('DELETE FROM presente WHERE codigo = ?',
+                [codigo], 
+                (error, results)=>{
+                    if(error){ rejeitado(error); return; }
+                        aceito(results);
+            })
+        })
+    },
+
+    //Mural
+
+    buscarTodosMural: ()=>{
+        return new Promise((aceito, rejeitado)=>{
+
+            db.query('SELECT * FROM mural', (error, results)=>{
+                if(error){ rejeitado(error); return; }
+                aceito(results)
+            });
+        });
+    },
+    buscarUmMural: (codigo) => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('SELECT * FROM mural WHERE codigo = ?', [codigo], (error, results)=>{
+                if(error){ rejeitado(error); return; }
+                if(results.length > 0){
+                    aceito(results[0]);
+                } else{
+                    aceito(false);
+                }
+            })
+        })
+    },
+    inserirMural: (nome, celular, parentesco, recado) => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('INSERT INTO mural (nome, celular, parentesco, recado)  VALUES (?,?,?,?)',
+                [nome, celular, parentesco, recado], 
+                (error, results)=>{
+                    if(error){ rejeitado(error); return; }
+                        aceito(results.insertCodigoMural);
+            })
+        })
+    },
+    alterarMural: (codigo, nome, celular, parentesco, recado) => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('UPDATE mural SET nome = ?, celular = ?, parentesco = ?, recado = ? WHERE codigo = ?',
+                [nome, celular, parentesco, recado, codigo], 
+                (error, results)=>{
+                    if(error){ rejeitado(error); return; }
+                        aceito(results);
+            })
+        })
+    },
+    excluirMural: (codigo) => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('DELETE FROM mural WHERE codigo = ?',
+                [codigo], 
+                (error, results)=>{
+                    if(error){ rejeitado(error); return; }
+                        aceito(results);
+            })
+        })
     }
 };
